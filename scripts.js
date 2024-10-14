cf94badfe49e35c87c7f573f1df9bb085fe029b9
Yovan Rodriguez <yovan.rodriguez244601@potros.itson.edu.mx>
2024-10-14 07:48:04 -0700

    Archivos iniciales del proyecto

---
+++ scripts.js
@@ -0,0 +1,5 @@
+// JavaScript básico para el botón de salir
+document.getElementById('salir-btn').addEventListener('click', function() {
+    alert('Saliendo de la página...');
+    window.location.href = 'login.html'; // Redirigir a la página de inicio de sesión o cerrar sesión
+});
